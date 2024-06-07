2166f69f9eb0c66df369ed3697cb9ab08ac68501
DanielGZamora <garzizamora@gmail.com>
2024-06-07 11:54:00 +0200

    added this meme

---
+++ html/pruebaCaptcha/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'ae2050bd93fb00e1d0bb5a5beedb87a12ab0c4f9',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'ae2050bd93fb00e1d0bb5a5beedb87a12ab0c4f9',
+            'dev_requirement' => false,
+        ),
+        'elioair/chesscaptcha' => array(
+            'pretty_version' => '2.0.2',
+            'version' => '2.0.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../elioair/chesscaptcha',
+            'aliases' => array(),
+            'reference' => 'e9a2bb9ff6ae9015ffacfff07405d904dc77156b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
